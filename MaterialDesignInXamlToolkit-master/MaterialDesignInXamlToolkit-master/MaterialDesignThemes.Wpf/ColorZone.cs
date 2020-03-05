1a98b05b4de53b4b74b5ec1bcc44cb6b3fb9bdbd
kyky404 <hanichkristof@gmail.com>
2020-03-05 16:51:25 +0100

    Material Design

---
+++ MaterialDesignInXamlToolkit-master/MaterialDesignInXamlToolkit-master/MaterialDesignThemes.Wpf/ColorZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public enum ColorZoneMode
+    {
+        Standard,
+        Inverted,
+        PrimaryLight,
+        PrimaryMid,
+        PrimaryDark,
+        Accent,
+        Light,
+        Dark,
+        Custom
+    }
+
+    /// <summary>
+    /// User a colour zone to easily switch the background and foreground colours, from selected Material Design palette or custom ones.
+    /// </summary>
+    public class ColorZone : ContentControl
+    {
+        static ColorZone()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorZone), new FrameworkPropertyMetadata(typeof(ColorZone)));
+        }
+
+        public static readonly DependencyProperty ModeProperty = DependencyProperty.Register(
+            nameof(Mode), typeof(ColorZoneMode), typeof(ColorZone), new PropertyMetadata(default(ColorZoneMode)));
+
+        public ColorZoneMode Mode
+        {
+            get { return (ColorZoneMode)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius), typeof(CornerRadius), typeof(ColorZone), new PropertyMetadata(default(CornerRadius)));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+    }
+}
