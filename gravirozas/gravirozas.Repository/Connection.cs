de21b2348c89296cf4e6932bd55ffdb1d9ddeec4
kyky404 <hanichkristof@gmail.com>
2020-03-12 12:01:06 +0100

    9,3k karakter

--- gravirozas/gravirozas.Repository/Connection.cs
+++ gravirozas/gravirozas.Repository/Connection.cs
@@ -7,7 +7,7 @@ namespace Gravirozas.Repository
     public static class Connection
     {
         public static string String { get; } = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=gravirozasDB;Integrated Security=SSPI;";
-        /*public static string String { get; } = @"Data Source=.\SQLEXPRESS;Initial Catalog=gravirozasDB;Integrated Security=SSPI;"; : MacBook*/
+        /*public static string String { get; } = @"Data Source=.\SQLEXPRESS;Initial Catalog=gravirozasDB;Integrated Security=SSPI;";*/
         
     }
 }
